783a9898af684ff7073aec6c1f609193ae52198f
RishiDev <73101003+Rishiraj0100@users.noreply.github.com>
2021-09-15 18:40:11 +0530

    Update and rename __init__.py to setup.py

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="shell-reader",
+    version='0.1.0',
+    author="Rishiraj0100",
+    description="A module for executing and reading shell.",
+    packages=find_packages(exclude=["docs", ".github", "examples", "tests"]),
+    classifiers=[
+        "Programming Language :: Python :: 3.8",
+        "License :: OSI Approved :: CC0-1.0 License",
+        "Operating System :: OS Independent",
+    ],
+    project_urls={
+        "Issue tracker": "https://github.com/Rishiraj0100/shell-reader/issues"
+    },
+    python_requires=">=3.8",
+    license="CC0-1.0"
+)
