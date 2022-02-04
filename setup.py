038e888513574d24016e992fa8cfd58e12b53944
RishiDev <73101003+Rishiraj0100@users.noreply.github.com>
2022-02-04 09:41:30 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="shell-reader",
-    version='0.1.1',
+    version='0.1.2',
     author="Rishiraj0100",
     description="A module for executing and reading shell.",
     packages=find_packages(exclude=["docs", ".github", "examples", "tests"]),
